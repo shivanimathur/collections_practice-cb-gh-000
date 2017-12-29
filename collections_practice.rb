9bcd8759b39d1b98917f519ae958cdd53cba08ad
Shivani Mathur <qwellery@gmail.com>
2017-12-29 07:10:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc
+  
+end
 
+def sort_array_desc
+  
+end
+
+def sort_array_char_count
+  
+end
+
+def swap_elements
+  
+end
+
+def reverse_array
+  
+end
+
+def kesha_maker
+  
+end
+
+def find_a
+  
+end
+
+def sum_array
+
+end
+
+def add_s
+  
+end
