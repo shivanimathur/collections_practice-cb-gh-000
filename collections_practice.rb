192f6e04c9de54270ff1e613096df3778f879a37
Shivani Mathur <qwellery@gmail.com>
2017-12-29 08:25:19 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.size <=> b.size
+  end
+end
+
+def swap_elements(array)
+  i = 0
+  while i < array.size
+    if i == 1
+      temp = array[1]
+      array[1] = array[2]
+      array [2] = temp
+    end
+    i += 1
+  end
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each_with_index do |e|
+    e[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |e|
+    e.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject (0) { |sum, i| sum + i}
+end
+
+def add_s(array)
+  array.each_with_index do |e, i|
+    if i != 1
+      e<<"s"
+    end
+  end
+end
