e8824430a8761233a2170240aa99b8ddee676d6b
Shivani Mathur <qwellery@gmail.com>
2017-12-29 07:33:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,7 +17,10 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-
+  i = 0
+  while i < array.size
+    
+  end
 end
 
 def reverse_array(array)
@@ -25,7 +28,7 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-
+  array
 end
 
 def find_a(array)
